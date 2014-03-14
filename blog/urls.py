c132516c3c189d50a41cd60b8a04b7ce78afd53f
Breno <brenotx@gmail.com>
2014-03-14 20:17:02 -0300

    Refactor tests/urls to use namespace

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^', include('core.urls')),
+	url(r'^', include('core.urls', namespace="core")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- core/tests.py
+++ core/tests.py
@@ -1,13 +1,20 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
 class HomePageViewTests(TestCase):
+    
+    def setUp(self):
+        self.url = reverse('core:home')
+        self.resp = self.client.get(self.url)
+    
+    def test_GET_homepage(self):
+        self.assertEqual(self.resp.status_code, 200)
 
-	def setUp(self):
-		self.resp = self.client.get('/')
+    def test_template_used(self):
+        self.assertTemplateUsed(self.resp, 'core/home.html')
 
-	def test_GET_homepage(self):
-		self.assertEqual(self.resp.status_code, 200)
+    def test_html5(self):
+        self.assertContains(self.resp, '<title>Welcome')
+        self.assertContains(self.resp, '<!DOCTYPE HTML>')
 
-	def test_template_used(self):
-		self.assertTemplateUsed(self.resp, 'core/home.html')
